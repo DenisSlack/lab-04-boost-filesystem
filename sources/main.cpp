c60d30066d48f5e83e68c4b4ebba6ecd44f39901
DenisSlack <denisslcak@gmail.com>
2020-01-14 21:33:49 +0300

    v5

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,9 +4,11 @@
 
 #include <iostream>
 
-#define path "/home/denis/CLionProjects/lab-04-boost-filesystem/misc/ftp"
+#define dpath "/home/denis/CLionProjects/lab-04-boost-filesystem/misc/ftp"
 
-int main() {
+int main(int argc, char *argv[]) {
+  boost::filesystem::path path = "misc/ftp";
+  if (argc > 1) path = argv[1];
   FTPA myanalis;
   myanalis.show_directory_analys(path);
   std::cout << "-----------------------------------------------\n";
